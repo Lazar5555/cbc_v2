cce048d3be51490d77fbf188447c2ef19104a867
Eleazar <poncebe@gmail.com>
2017-05-25 20:49:42 -0500

    Permissions for manage categories was added. Category index, show, edit with style now.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,7 +2,7 @@ module RecipesHelper
   def youtube_embed youtube_url
     youtube_id = (/([\w-]{11})/.match(youtube_url)).to_s
 
-    html = "<iframe id='ytplayer' type='text/html' width='640' height='360' src='http://www.youtube.com/embed/#{youtube_id}?autoplay=1&origin=http://example.com' frameborder='0'/>"
+    html = "<iframe id='ytplayer' type='text/html' width='640' height='360' src='http://www.youtube.com/embed/#{youtube_id}?autoplay=0&origin=http://example.com' frameborder='0'/>"
     html.html_safe
   end
 end
