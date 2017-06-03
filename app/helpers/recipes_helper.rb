4b195fdd8a71963e52d2805255efdd22d73fe845
Eleazar <poncebe@gmail.com>
2017-06-02 21:59:40 -0500

    Views for Recipes are done for all CRUD methods.

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,7 +2,7 @@ module RecipesHelper
   def youtube_embed youtube_url
     youtube_id = (/([\w-]{11})/.match(youtube_url)).to_s
 
-    html = "<iframe id='ytplayer' type='text/html' width='920' height='500' src='http://www.youtube.com/embed/#{youtube_id}?autoplay=0&origin=http://example.com' frameborder='0' allowfullscreen='allowfullscreen'></iframe>"
+    html = "<iframe id='ytplayer' type='text/html' width='100%' height='500' src='http://www.youtube.com/embed/#{youtube_id}?autoplay=0&origin=http://example.com' frameborder='0' allowfullscreen='allowfullscreen'></iframe>"
     html.html_safe
   end
 end
