c9dc09ded7395f8cfaef69d332a3fb892e6438e8
Eleazar <poncebe@gmail.com>
2017-06-08 19:02:10 -0500

    New styles for devise views was added. Dashboard view is done.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
+  before_action :authenticate_admin!, only: [:dashboard]
   def index
 
   end
